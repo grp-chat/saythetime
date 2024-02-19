070c7bdbee9f2fd77a41b34c1cb072d81c3cc0f9
andrew <julieyeng.js@gmail.com>
2024-02-19 23:14:59 +0700

    p

--- client/js/index.js
+++ client/js/index.js
@@ -23,9 +23,7 @@ const HOURS = [
 
 // alert("test");
 
-const timeNowHours = new Date().getHours();
-const timeNowMinutes = new Date().getMinutes();
-const timeNowInHHMM = [timeNowHours,timeNowMinutes].join(':');
+
 
 function convertToAMPM(time) {
     const [h, m] = time.split(':').map(n => parseInt(n));
@@ -34,15 +32,18 @@ function convertToAMPM(time) {
     return [hour, suffix];
 }
 
-const timeNowHoursInAMPM = convertToAMPM(timeNowInHHMM);
-
 // console.log(talk(timeNowInHHMM));
 
-const speech = new SpeechSynthesisUtterance(
-    `Hello, the time now is ${timeNowHoursInAMPM[0]} ${timeNowMinutes} ${timeNowHoursInAMPM[1]}`
-);
-
 setInterval(() => {
+    const timeNowHours = new Date().getHours();
+    const timeNowMinutes = new Date().getMinutes();
+    const timeNowInHHMM = [timeNowHours,timeNowMinutes].join(':');
+    const timeNowHoursInAMPM = convertToAMPM(timeNowInHHMM);
+
+    const speech = new SpeechSynthesisUtterance(
+        `Hello, the time now is ${timeNowHoursInAMPM[0]} ${timeNowMinutes} ${timeNowHoursInAMPM[1]}`
+    );
+
     speechSynthesis.speak(speech);
 }, 60000);
 

37e338dfad3f836e5dc3755cef60780a80aaf4e8
andrew <julieyeng.js@gmail.com>
2024-09-03 22:48:11 +0700

    push

--- client/js/index.js
+++ client/js/index.js
@@ -25,46 +25,60 @@ const HOURS = [
 
 // alert("test");
 
-const requestWakeLock = async () => {
-    let wakeLock = null;
-    try {
-        wakeLock = await navigator.wakeLock.request('screen');
-        wakeLock.addEventListener('release', () => {
-            console.log('Wake Lock was released');
-        });
-        console.log('Wake Lock is active');
-    } catch (err) {
-        console.error(`${err.name}, ${err.message}`);
-    }
-};
 
-requestWakeLock();
+// const requestWakeLock = async () => {
+//     let wakeLock = null;
+//     try {
+//         wakeLock = await navigator.wakeLock.request('screen');
+//         wakeLock.addEventListener('release', () => {
+//             console.log('Wake Lock was released');
+//         });
+//         console.log('Wake Lock is active');
+//     } catch (err) {
+//         console.error(`${err.name}, ${err.message}`);
+//     }
+// };
+
+//requestWakeLock();
 
 
 
 function convertToAMPM(time) {
     const [h, m] = time.split(':').map(n => parseInt(n));
     const hour = HOURS[h % 12];
-    const suffix = (h < 12) ? 'A.M' : 'P.M';
+    const suffix = (h < 12) ? 'ayy,m' : 'P...M';
     return [hour, suffix];
 }
 
 // console.log(talk(timeNowInHHMM));
 
-setInterval(() => {
+function sayTheTime () {
     const timeNowHours = new Date().getHours();
     const timeNowMinutes = new Date().getMinutes();
     const timeNowInHHMM = [timeNowHours,timeNowMinutes].join(':');
     const timeNowHoursInAMPM = convertToAMPM(timeNowInHHMM);
 
     const speech = new SpeechSynthesisUtterance(
-        `Hello, the time now is ${timeNowHoursInAMPM[0]} ${timeNowMinutes} ${timeNowHoursInAMPM[1]}`
+        `Hello, the time now is: ${timeNowHoursInAMPM[0]} ${timeNowMinutes}, ${timeNowHoursInAMPM[1]}`
     );
 
     speechSynthesis.speak(speech);
+}
+
+setTimeout(() => {
+    sayTheTime();
+    sayTheTime();
+    
+}, 5000);
+
+
+setInterval(() => {
+    sayTheTime();
+    sayTheTime();
     // requestWakeLock();
 }, 60000);
 
 setInterval(() => {
+    // sayTheTime();
     console.log("running");
 }, 5000);

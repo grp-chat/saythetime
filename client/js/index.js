1996bb785d2579dc0dbc7ae1f42029d201e0a35e
andrew <julieyeng.js@gmail.com>
2025-06-05 17:48:11 +0700

    display time

--- client/js/index.js
+++ client/js/index.js
@@ -15,46 +15,28 @@ const HOURS = [
     "eleven"
 ];
 
-// if ('wakeLock' in navigator) {alert("yes")}
-
-// async function acquireLock() {
-//     await navigator.wakeLock.request("screen");
-// }
-
-// acquireLock();
-
-// alert("test");
-
-
-// const requestWakeLock = async () => {
-//     let wakeLock = null;
-//     try {
-//         wakeLock = await navigator.wakeLock.request('screen');
-//         wakeLock.addEventListener('release', () => {
-//             console.log('Wake Lock was released');
-//         });
-//         console.log('Wake Lock is active');
-//     } catch (err) {
-//         console.error(`${err.name}, ${err.message}`);
-//     }
-// };
-
-//requestWakeLock();
-
-
 
 function convertToAMPM(time) {
     const [h, m] = time.split(':').map(n => parseInt(n));
     const hour = HOURS[h % 12];
     const suffix = (h < 12) ? 'ayy,m' : 'P.M';
-    return [hour, suffix];
+    const hourInNum = (h < 12) ? h : h - 12;
+    return [hour, suffix, hourInNum];
 }
 
-// console.log(talk(timeNowInHHMM));
-
-function sayTheTime () {
+function getTheTime() {
     const timeNowHours = new Date().getHours();
     const timeNowMinutes = new Date().getMinutes();
+    const timeNowSeconds = new Date().getSeconds();
+
+    return [timeNowHours, timeNowMinutes, timeNowSeconds];
+
+}
+
+function sayTheTime() {
+
+    const [timeNowHours, timeNowMinutes] = getTheTime();
+    
     const timeNowInHHMM = [timeNowHours,timeNowMinutes].join(':');
     const timeNowHoursInAMPM = convertToAMPM(timeNowInHHMM);
 
@@ -63,6 +45,17 @@ function sayTheTime () {
     );
 
     speechSynthesis.speak(speech);
+}
+
+function displayTime() {
+    const [timeNowHours, timeNowMinutes, timeNowSeconds] = getTheTime();
+    const timeNowInHHMM = [timeNowHours,timeNowMinutes].join(':');
+    const timeNowHoursInAMPM = convertToAMPM(timeNowInHHMM);
+
+    document.getElementById('hours').innerHTML = timeNowHoursInAMPM[2];
+    document.getElementById('minutes').innerHTML = timeNowMinutes;
+    document.getElementById('seconds').innerHTML = timeNowSeconds;
+    document.getElementById('session').innerHTML = timeNowHoursInAMPM[1];
 }
 
 setTimeout(() => {
@@ -75,10 +68,10 @@ setTimeout(() => {
 setInterval(() => {
     sayTheTime();
     sayTheTime();
-    // requestWakeLock();
 }, 60000);
 
 setInterval(() => {
-    // sayTheTime();
     console.log("running");
 }, 5000);
+
+setInterval(displayTime, 1000);

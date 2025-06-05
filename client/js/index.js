44e49a71656aca6f47e8935b299d26432aa5d209
andrew <julieyeng.js@gmail.com>
2025-06-06 00:56:46 +0700

    count down timer

--- client/js/index.js
+++ client/js/index.js
@@ -16,6 +16,17 @@ const HOURS = [
 ];
 
 
+const count = 60;
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
+const countdown = document.querySelector(".remaining");
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+    remainingTime(count);
+    sayTheTime();
+});
+
+
 function convertToAMPM(time) {
     const [h, m] = time.split(':').map(n => parseInt(n));
     const hour = HOURS[h % 12];
@@ -47,25 +58,70 @@ function sayTheTime() {
     speechSynthesis.speak(speech);
 }
 
-function displayTime() {
-    const [timeNowHours, timeNowMinutes, timeNowSeconds] = getTheTime();
-    const timeNowInHHMM = [timeNowHours,timeNowMinutes].join(':');
-    const timeNowHoursInAMPM = convertToAMPM(timeNowInHHMM);
 
-    document.getElementById('hours').innerHTML = timeNowHoursInAMPM[2];
-    document.getElementById('minutes').innerHTML = timeNowMinutes;
-    document.getElementById('seconds').innerHTML = timeNowSeconds;
-    document.getElementById('session').innerHTML = timeNowHoursInAMPM[1];
+function formatTime(date){
+    
+    const hours12 = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const isAM = date.getHours() < 12;
+
+    return `${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")} ${isAM ? "AM":"PM"}`
 }
 
-setTimeout(() => {
-    sayTheTime();
-    sayTheTime();
+function formatDate(date) {
+    const DAYS = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  return `${DAYS[date.getDay()]}, ${date.getDate()} ${MONTHS[date.getMonth()]} ${date.getFullYear()} `
+
+
+}
+
+// setTimeout(() => {
+//     sayTheTime();
+//     sayTheTime();
     
-}, 5000);
+// }, 5000);
+
+function remainingTime (count) {
+    const interval = setInterval(() => {
+        count--;
+        countdown.textContent = "Speaking again in " + count + " sec";
+        if (count == 0) {
+            count = 60;
+            clearInterval(interval);
+        }
+    },1000);
+}
 
 
 setInterval(() => {
+    
+    var count = 60
+    remainingTime(count);
+    
     sayTheTime();
     sayTheTime();
 }, 60000);
@@ -74,4 +130,8 @@ setInterval(() => {
     console.log("running");
 }, 5000);
 
-setInterval(displayTime, 1000);
+setInterval(() => {
+    const date = new Date;
+    timeElement.textContent = formatTime(date);
+    dateElement.textContent = formatDate(date);
+}, 1000)
